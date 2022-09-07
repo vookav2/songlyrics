1e88d51f8c047b98a7c226c3ec53bbec6aea53d3
Eitho <62159998+Eithoo@users.noreply.github.com>
2022-09-07 05:06:58 +0200

    fixed AZLyrics empty lyrics string error (#18)
    * fixed AZLyrics empty lyrics string error
    
    * Update package.json
    
    * Update package.json
    
    Co-authored-by: Davi Nomoeh Dani <davinomoehdanino@gmail.com>

--- src/sources/azlyrics.ts
+++ src/sources/azlyrics.ts
@@ -3,9 +3,12 @@ import { HTMLElement } from 'node-html-parser'
 export const azlyrics = {
   name: 'AZLyrics',
   parse: (html: HTMLElement): Promise<string> => {
-    const content = html
+    let possibleContent = html
       .querySelector('div.ringtone')
-      ?.nextElementSibling.nextElementSibling.nextElementSibling.nextElementSibling.textContent.trim()
+      ?.nextElementSibling.nextElementSibling.nextElementSibling.nextElementSibling
+    if (html.querySelector('span.feat'))
+        possibleContent = possibleContent?.nextElementSibling.nextElementSibling
+    const content = possibleContent?.textContent.trim()
     return Promise.resolve(`${content}`)
   },
 }
